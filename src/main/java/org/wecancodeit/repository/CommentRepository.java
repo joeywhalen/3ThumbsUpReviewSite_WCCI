2415607c08188117cc480af7adeae122af124cfa
dasminew5 <dasminewright5@gmail>
2021-06-30 15:12:38 -0400

    fixed comment issue

--- src/main/java/org/wecancodeit/repository/CommentRepository.java
+++ src/main/java/org/wecancodeit/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package org.wecancodeit.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.wecancodeit.pojos.Comment;
 
-public interface CommentRepository extends CrudRepository {
+public interface CommentRepository extends CrudRepository<Comment, Long> {
 }
